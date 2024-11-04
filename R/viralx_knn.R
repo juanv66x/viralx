799ae3d991956a86ed31e213ea2cfc86f6a147a4
Juan Acuna <acua6307@gmail.com>
2024-11-03 18:11:11 -0600

    Adding SHAP functionalities to KNN explainer

--- R/viralx_knn.R
+++ R/viralx_knn.R
@@ -18,10 +18,9 @@
 #' @export
 #'
 #' @examples
-#' set.seed(123)
 #' hiv_data <- train2
 #' knn_hyperparameters <- list(neighbors = 5, weight_func = "optimal", dist_power = 0.3304783)
-#' vip_featured <- c("cd_2022")
+#' vip_featured <- "cd_2022"
 #' vip_train <- hiv_data
 #' vip_new <- vip_train[1,]
 #' viralx_knn(vip_featured, hiv_data, knn_hyperparameters, vip_train, vip_new)
--- tests/testthat/test-viralx_knn.R
+++ tests/testthat/test-viralx_knn.R
@@ -1,9 +1,8 @@
 test_that("`viralx_knn()` works", {
   local_edition(3)
-  set.seed(123)
   hiv_data <- train2
   knn_hyperparameters <- list(neighbors = 5, weight_func = "optimal", dist_power = 0.3304783)
-  vip_featured <- c("cd_2022")
+  vip_featured <- "cd_2022"
   vip_train <- hiv_data
   vip_new <- vip_train[1,]
   expect_snapshot(print(viralx_knn(vip_featured, hiv_data, knn_hyperparameters, vip_train, vip_new)))
